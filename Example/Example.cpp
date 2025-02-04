7593e76b08504f3c896b97edd1002eb05a55903e
Ivan Smirnov <i.smirnov@1440.space>
2025-02-04 08:42:53 +0300

    array && vector

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -3,9 +3,65 @@
 
 #include <iostream>
 
+template<typename T, int Size>
+struct Array {
+    T& operator [] (int idx) { return _arr[idx]; }
+    T _arr[Size];
+};
+
+template<typename T>
+class Vector {
+public:
+    Vector() {}
+    Vector(int size) : _size(size), _capacity(size) { _arr = new T[_size]; }
+    ~Vector() { delete[] _arr; }
+    T& operator [] (int idx) { return _arr[idx]; }
+    void push_back(const T& elem)
+    {
+        if (_size == _capacity)
+        {
+            T* tmp = new T[_size];
+            for (int idx = 0; idx < _size; ++idx) { tmp[idx] = _arr[idx]; }
+            delete[] _arr;
+            _capacity = _capacity * 2;
+            _arr = new T[_capacity];
+            for (int idx = 0; idx < _size; ++idx) { _arr[idx] = tmp[idx]; }
+            delete[] tmp;
+            _arr[_size] = elem;
+            ++_size;
+        }
+        else
+        {
+            _arr[_size] = elem;
+            ++_size;
+        }
+            
+    }
+
+    int size() const { return _size; }
+    int capacity() const { return _capacity; }
+private:
+    int _size = 1;
+    int _capacity = 1;
+    T* _arr;
+};
+
+// MyArray deduction guides:
+// similar code was added to std::array in C++17 to allow the
+// creation of a2 below, without providing template arguments
+//template <class _First, class... _Rest>
+//MyArray(_First, _Rest...) -> MyArray<_First, 1 + sizeof...(_Rest)>;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Array<int, 5> myArr = {1, 2, 3, 4, 5};
+    myArr[2] = 1;
+    std::cout << myArr[2] << std::endl;
+
+    Vector<float> v(5);
+    std::cout << "Size: " << v.size() << ", capacity: " << v.capacity() << std::endl;
+    v.push_back(1);
+    std::cout << "Size: " << v.size() << ", capacity: " << v.capacity() << std::endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
