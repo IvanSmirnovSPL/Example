cd30c492364aa5a084b4a98f297bc63e970286d6
Ivan Smirnov <i.smirnov@1440.space>
2025-02-05 11:38:06 +0300

    initializer_list

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -2,33 +2,64 @@
 //
 
 #include <iostream>
+#include <algorithm>
 
 template<typename T, int Size>
-struct Array {
+class Array {
+public:
+    Array(): _size(Size) {};
+    Array(std::initializer_list<T> l) : _size(Size)
+    {
+        auto iter = l.begin();
+        for (int idx = 0; idx < _size; ++idx)
+            _arr[idx] = *iter++;
+    }
+    ~Array() = default;
+    int size() const { return _size; }
     T& operator [] (int idx) { return _arr[idx]; }
+private:
+    int _size;
     T _arr[Size];
 };
 
 template<typename T>
 class Vector {
 public:
-    Vector() {}
+    Vector(): _size(0), _capacity(0) {}
     Vector(int size) : _size(size), _capacity(size) { _arr = new T[_size]; }
+    Vector(std::initializer_list<T> l): _size(l.size()), _capacity(l.size())
+    {
+        _arr = new T[_size];
+        //std::copy(l.begin(), l.end(), _arr);
+        int arrIdx = 0;
+        for (auto iter = l.begin(); iter != l.end(); ++iter)
+        {
+            _arr[arrIdx++] = *iter;
+        }
+    }
     ~Vector() { delete[] _arr; }
     T& operator [] (int idx) { return _arr[idx]; }
+    T  operator [] (int idx) const { return _arr[idx]; }
     void push_back(const T& elem)
     {
         if (_size == _capacity)
         {
-            T* tmp = new T[_size];
-            for (int idx = 0; idx < _size; ++idx) { tmp[idx] = _arr[idx]; }
-            delete[] _arr;
-            _capacity = _capacity * 2;
-            _arr = new T[_capacity];
-            for (int idx = 0; idx < _size; ++idx) { _arr[idx] = tmp[idx]; }
-            delete[] tmp;
-            _arr[_size] = elem;
-            ++_size;
+            if (_size > 0)
+            {
+                T* tmp = new T[_size];
+                for (int idx = 0; idx < _size; ++idx) { tmp[idx] = _arr[idx]; }
+                delete[] _arr;
+                _capacity = _capacity * 2;
+                _arr = new T[_capacity];
+                for (int idx = 0; idx < _size; ++idx) { _arr[idx] = tmp[idx]; }
+                delete[] tmp;
+            }
+            else
+            {
+                _capacity = 1;
+                _arr = new T[_capacity];
+            }
+            _arr[_size++] = elem;
         }
         else
         {
@@ -38,6 +69,15 @@ public:
             
     }
 
+    friend std::ostream& operator << (std::ostream& out, const Vector<T>& vec)
+    {
+        out << "[";
+        for (int idx = 0; idx < vec.size(); ++idx)
+            out << vec[idx] << ", ";
+        out << "]";
+        return out;
+    }
+
     int size() const { return _size; }
     int capacity() const { return _capacity; }
 private:
@@ -45,12 +85,6 @@ private:
     int _capacity = 1;
     T* _arr;
 };
-
-// MyArray deduction guides:
-// similar code was added to std::array in C++17 to allow the
-// creation of a2 below, without providing template arguments
-//template <class _First, class... _Rest>
-//MyArray(_First, _Rest...) -> MyArray<_First, 1 + sizeof...(_Rest)>;
 
 int main()
 {
@@ -62,6 +96,18 @@ int main()
     std::cout << "Size: " << v.size() << ", capacity: " << v.capacity() << std::endl;
     v.push_back(1);
     std::cout << "Size: " << v.size() << ", capacity: " << v.capacity() << std::endl;
+
+    Vector<double> w;
+    std::cout << "Size: " << w.size() << ", capacity: " << w.capacity() << std::endl;
+    w.push_back(1);
+    std::cout << "Size: " << w.size() << ", capacity: " << w.capacity() << std::endl;
+    w.push_back(10);
+    std::cout << "Size: " << w.size() << ", capacity: " << w.capacity() << std::endl;
+    w.push_back(100);
+    std::cout << "Size: " << w.size() << ", capacity: " << w.capacity() << std::endl;
+
+    Vector<int> u = { 1, 2 };
+    std::cout << u << std::endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
