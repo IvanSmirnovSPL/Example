6e5e33fd7dfbda3a2f35c7406cf0e81abbe1bfaa
Ivan Smirnov <i.smirnov@1440.space>
2025-02-11 08:47:44 +0300

    строки

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -2,19 +2,92 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string message;
+    std::string message1{"ABC"};
+    std::string message2 = { "TMP" };
+    std::string message3("RRRR");
+    std::string message4(5, 'Q'); // QQQQQ
+    std::string message5{ "hello world", 4 }; // берем первые 4 символа - message5 = hell
+    std::string message6{ "hello world", 6, 5 }; // 5 символов начиная с 6 символа - message6 = world
+
+    // считывание всей строки 
+    std::string name;
+    std::cout << "Input your name: ";
+    getline(std::cin, name);
+    std::cout << "Your name: " << name << std::endl;
+
+    // размер строки
+    std::string message7{ "Hello" };
+    std::cout << "Length: " << message7.length() << std::endl;   // Length: 5
+    std::cout << "Size: " << message7.size() << std::endl;       // Size: 5
+
+    // пустая ли строка
+    std::string message8;
+    if (message8.empty())
+        std::cout << "string is empty " << std::endl;
+    else
+        std::cout << "string is not empty" << std::endl;
+
+    // объединение строк
+    std::string hello{ "hello" };
+    std::string world{ "world" };
+    std::string msg{ hello + " " + world };
+    std::cout << msg << std::endl;    // hello world
+    // std::string tmp{ "hello " + "world" + "!"};   // ! Ошибка
+    // std::string tmp{ hello + " world" "!" };   // hello world!
+    using namespace std::string_literals; // строки в std::string
+    std::string foo{ "hello "s + "world"s + "!"s };
+
+    // спец символы
+    std::string text{ "Name: \t\"Tom\"\nAge:\t38" };
+    std::cout << text << std::endl;
+
+    // raw
+    std::string text2
+    { R"(Name:   "Tom"
+         Age:    38)" };
+    std::cout << text2 << std::endl;
+
+    // методы строк
+    std::string test{"Too much"};
+    test.resize(5);
+    std::cout << test << " " << test.size() << std::endl;
+    test.resize(10);
+    std::cout << test << " " << test.size() << " '" << test[9] << "' " << std::endl;
+    test.resize(12, 'w');
+    std::cout << test << " " << test.size() << std::endl;
+    // .clear(), .push_back('a')
+    test.append("FEFM"); // .append(n, c), .append(T, pos, count)
+    std::cout << test << " " << test.size() << std::endl;
+    // S.erase(pos) - удаляет из строки S  с символа с индексом pos и до конца строки.
+    // S.erase(pos, count) - удаляет из строки S  с символа с индексом pos количеством count или до конца строки, если pos + count > S.size().
+    /*
+     S.insert(i, n, c) - вставить n одинаковых символов, равных с. n имеет целочисленный тип, c - char.
+     S.insert(i, T) - вставить содержимое строки T. T может быть объектом класса string или C-строкой.
+     S.insert(i, T, pos, count) - вставить символы строки T начиная с символа с индексом pos количеством count.
+     
+     S.substr(pos) - возвращает подстроку данной строки начиная с символа с индексом pos и до конца строки.
+     S.substr(pos, count) - возвращает подстроку данной строки начиная с символа с индексом pos количеством count или до конца строки, если pos + count > S.size().
+     
+     S.replace(pos, count, n, c) - вставить n одинаковых символов, равных с. n имеет целочисленный тип, c - char.
+     S.replace(pos, count, T) - вставить содержимое строки T. T может быть объектом класса string или C-строкой.
+     S.replace(pos, count, T, pos2, count2) - вставить символы строки T начиная с символа с индексом pos количеством count.
+    
+    S.find(str, pos = 0) - искать первое входение строки str начиная с позиции pos. Если pos не задано - то начиная с начала строки S.
+    S.find(str, pos, n) - искать в данной строке подстроку, равную первым n символам строки str. Значение pos должно быть задано.
+
+    rfind: Ищет последнее вхождение подстроки ("правый" поиск). Способы вызова аналогичны способам вызова метода find.
+
+    S.find_first_of(str, pos = 0) - искать первое входение любого символа строки str начиная с позиции pos. Если pos не задано - то начиная с начала строки S.
+    
+    find_last_of: Ищет в данной строке последнее появление любого из символов данной строки str.
+
+    find_first_not_of / find_last_not_of
+
+    c_str: Возвращает указать на область памяти, в которой хранятся символы строки, возвращает значение типа char*. Возвращаемое значение можно рассматривать как C-строку и использовать в функциях, которые должны получать на вход C-строку.
+    */
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
