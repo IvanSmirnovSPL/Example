2c35d92b4fa9d1c8edd2703494a212d0efe78ab6
Ivan Smirnov <i.smirnov@1440.space>
2025-03-29 15:20:32 +0300

    filesystem

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -2,19 +2,44 @@
 //
 
 #include <iostream>
+#include <filesystem>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru_RU");
+
+    std::string someString{ R"(C:\Workspace\с++ practice\Example\Example)" };
+    std::filesystem::path somePath(someString);
+    std::cout << "somePath: " << somePath.string() << std::endl;
+
+    std::filesystem::path curPath = std::filesystem::current_path();
+    std::cout << "curPath: " << curPath.string() << std::endl;
+
+    auto parent = curPath.parent_path();
+    std::cout << "Parent: " << parent.string() << std::endl;
+
+    auto myDir = parent / "myDir";
+    std::cout << "MyDir: " << myDir.string() << std::endl;
+
+    std::cout << std::endl;
+    for (auto& line : std::filesystem::directory_iterator(myDir))
+    {
+        std::cout << "Path: " << line.path().string() << ", filename: "
+            << line.path().filename().string() << std::endl;
+    }
+    std::cout << std::endl;
+
+    auto fooPath = myDir / "foo.txt";
+    if (std::filesystem::exists(fooPath))
+    {
+        std::filesystem::remove(fooPath);
+    }
+    std::filesystem::copy(myDir / "tmp.txt", myDir / "foo.txt");
+
+    auto barPath = myDir / "bar.txt";
+    if (std::filesystem::exists(barPath))
+    {
+        std::filesystem::remove(barPath);
+    }
+    std::filesystem::rename(fooPath, barPath);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
