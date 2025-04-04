78739da809cbd89bfd308db4ff22af0d657c8d66
Ivan Smirnov <i.smirnov@1440.space>
2025-04-04 22:00:02 +0300

    методы класса

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -1,30 +1,46 @@
 ﻿#include <iostream>
 #include <thread>
 #include <chrono>
+#include <functional>
 
 using namespace std::chrono_literals;
 
-// 1. возврат параметров
+// 1. передача методов класса
 
-double someWork(int a, double b)
+class MyClass
 {
-    std::cout << "Начало работы, id: " << std::this_thread::get_id() << std::endl;
-    std::this_thread::sleep_for(3s);
-    double res = a + b;
-    std::cout << "a + b = " << res << std::endl;
-    std::this_thread::sleep_for(3s);
-    std::cout << "Конец работы, id: " << std::this_thread::get_id() << std::endl;
-    return res;
-}
+public:
+    double someWork(int a, double b)
+    {
+        std::cout << "Начало работы, id: " << std::this_thread::get_id() << std::endl;
+        std::this_thread::sleep_for(3s);
+        double res = a + b;
+        std::cout << "a + b = " << res << std::endl;
+        std::this_thread::sleep_for(3s);
+        std::cout << "Конец работы, id: " << std::this_thread::get_id() << std::endl;
+        return res;
+    }
+
+    void bar() {}
+};
 
 int main()
 {
     setlocale(LC_ALL, "ru");
 
-    double c = 1;
+    double c = 0;
     std::cout << "C: " << c << std::endl;
-    std::thread th([&c]() { c = someWork(2, 5); });
-    //std::thread th([&c](int a, double b) { c = someWork(a, b); }, 2, 5);
+
+
+    MyClass tmp;
+    std::thread th([&c, &tmp]() { c = tmp.someWork(2, 5); });
+
+    //auto f = std::bind(&MyClass::someWork, &tmp, std::placeholders::_1, std::placeholders::_2);
+    //std::thread th(
+    //    [&c, &tmp](std::function<double(int, double)> ff, int a, double b) { c = ff(a, b); },
+    //    f, 2, 5
+    //);
+
 
     for (int i = 0; i < 10; ++i)
     {
