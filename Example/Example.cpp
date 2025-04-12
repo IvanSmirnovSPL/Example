fec3f6cbd9be465c8f7d9c88da101fd171109865
Ivan Smirnov <i.smirnov@1440.space>
2025-04-12 16:42:01 +0300

    пример 2

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -14,56 +14,31 @@
 // 4. recursive_lock, unick_lock
 // 5. barrier std::barrier my_barrier{ 3 }; my_barrier.arrive_and_wait(); latch 
 
-double pi = 3.14159265358;
-
 std::mutex mtx1;
-std::mutex mtx;
+std::mutex mtx2;
 
-double func(double x)
+void doWork1()
 {
-    double y = 5 * x * x * std::cos(x);
-    return y;
+    mtx1.lock();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    mtx2.lock();
+    mtx2.unlock();
+    mtx1.unlock();
 }
 
-void integrate(
-    double a,
-    double b,
-    std::function<double(double)> f,
-    double& res
-)
+void doWork2()
 {
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    int N = 1000000; // кол-во отрезков разбиения
-    double step = std::abs(b - a) / N;
-    double tmp = 0;
-    for (int i = 0; i < N; ++i)
-    {
-        tmp += step * f(std::min(a, b) + step * i);
-    }
-    mtx.lock();
-    res += tmp;
-    mtx.unlock();
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    mtx2.lock();
+    mtx1.lock();
+    mtx1.unlock();
+    mtx2.unlock();
 }
 
 int main()
 {
-    auto t = std::chrono::system_clock::now();
-    double res = 0;
-    //integrate(- pi, pi, func, std::ref(res));
-
-
-    //integrate(-pi, 0, func, std::ref(res));
-    //integrate(0, pi, func, std::ref(res));
-
-    std::thread th1(integrate, -pi, 0, func, std::ref(res));
-    std::thread th2(integrate, 0, pi, func, std::ref(res));
+    std::thread th1(doWork1);
+    std::thread th2(doWork2);
     
     th1.join();
     th2.join();
-
-    std::cout << res << std::endl;
-
-    using dseconds = std::chrono::duration<double, std::chrono::seconds::period>;
-    std::cout << dseconds{std::chrono::system_clock::now() - t}.count() << std::endl;
 }
